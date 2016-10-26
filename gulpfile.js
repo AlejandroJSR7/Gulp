d0195e5d68c8e6b74b3d33566fecddefcf4dd6cc
AlejandroJSR7 <AlejandroJSR7@Gmail.com>
2016-10-26 13:25:34 -0500

    Gulp + BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function(){
-    console.log('Hola Gulp!!!');
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
+// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function(){
     return gulp.src('scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('app/css'));
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function(){
